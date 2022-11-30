19afac4c85d8eae8ba76eafbabd4fb8e68aa57d3
KarolRadomski <77892318+KarolRadomski@users.noreply.github.com>
2022-11-30 13:39:29 +0100

    init3

--- API/server.js
+++ API/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'));
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
 }
 
 app.listen(port, () => console.log(`Server is up on http://localhost:${port}`));
