fbf50fe1fc4af7e881b890cce165cb9a19cd3326
KarolRadomski <77892318+KarolRadomski@users.noreply.github.com>
2022-11-30 14:53:45 +0100

    init4

--- API/server.js
+++ API/server.js
@@ -22,10 +22,10 @@ app.use('/download', require('./Routes/downloadRoutes'));
 // Handle production
 if (process.env.NODE_ENV === 'production') {
   //Static folder
-  app.use(express.static(__dirname + '/public/'));
+  app.use(express.static(path.join(__dirname, 'public/')));
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/index.html')));
 }
 
 app.listen(port, () => console.log(`Server is up on http://localhost:${port}`));
