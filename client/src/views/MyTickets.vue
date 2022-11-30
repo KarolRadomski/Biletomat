a2071fab4b08c01a84ad293bf822575a7ae323af
KarolRadomski <77892318+KarolRadomski@users.noreply.github.com>
2022-11-30 15:27:10 +0100

    init5

--- client/src/views/MyTickets.vue
+++ client/src/views/MyTickets.vue
@@ -39,9 +39,9 @@
                   </div>
                   <div class="price">{{ ticket.seatInSector.sectorDetail.price }} zł</div>
                   <div class="download">
-                    <!-- <a :href="'http://localhost:5000/ticket/bilet-' + ticket.id + '.pdf'" target="_blank">
+                    <a :href="'https://biletomat-karradomski.herokuapp.com/ticket/bilet-' + ticket.id + '.pdf'" target="_blank">
                       <button class="previewButton">Podgląd<i class="bi bi-eye-fill"></i></button>
-                    </a> -->
+                    </a>
                     <a :href="'https://biletomat-karradomski.herokuapp.com/download/Bilet-' + ticket.id + '.pdf'">
                       <button class="downloadButton">Pobierz bilet <i class="bi bi-download"></i></button>
                     </a>
