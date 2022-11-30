8b42914dd86eba1b44c7b4f5915d0ab24cec930e
KarolRadomski <77892318+KarolRadomski@users.noreply.github.com>
2022-11-30 15:21:50 +0100

    init5

--- API/server.js
+++ API/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); //Allow to read uploaded file by http://localhost:5000/uploads/sektory.png
-// app.use('/ticket', express.static(path.join(__dirname, 'tickets')));
+app.use('/ticket', express.static(path.join(__dirname, 'tickets')));
 
 app.use('/API/auth', require('./Routes/authRoutes'));
 app.use('/API/event', require('./Routes/eventRoutes'));
